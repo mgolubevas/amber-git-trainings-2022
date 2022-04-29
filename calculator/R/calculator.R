a4c9aebd7abd396252e77a6a81880d0804d94616
Michailas Golubevas <michailas.golubevas@gjensidige.no>
2022-04-29 12:12:58 +0200

    Creating repository

---
+++ calculator/R/calculator.R
@@ -0,0 +1,18 @@
+# Simple Calculator Implementation in R
+
+
+add <- function(arg_1, arg_2) {
+   return(arg_1 + arg_2)
+}
+
+subtract <- function(arg_1, arg_2) {
+   return(arg_1 - arg_2)
+}
+
+multiply <- function(arg_1, arg_2) {
+   return(arg_1 * arg_2)
+}
+
+divide <- function(arg_1, arg_2) {
+   return(arg_1/arg_2)
+}
