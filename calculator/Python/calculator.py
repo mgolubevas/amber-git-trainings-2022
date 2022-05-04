6ea6306d1c61fcafb7754bd207d8f5618000eed6
Gytaute Balioniene <gytaute.balioniene@gjensidige.lt>
2022-05-04 12:44:38 +0300

    Pakeitimai pridejus visur 1

--- calculator/Python/calculator.py
+++ calculator/Python/calculator.py
@@ -2,17 +2,13 @@
 
 
 def add(arg1, arg2):
-  #   Your code goes here
-  return((arg1 + arg2) / 2)
+  return((arg1 + arg2) / 2 +1)
 
 def subtract(arg1, arg2):
-  #   Your code goes here
-  return(arg1 - arg2)
+  return(arg1 - arg2 +1)
 
 def multiply(arg1, arg2):
-  #   Your code goes here
-  return(arg1 * arg2)
+  return(arg1 * arg2 +1)
 
 def divide(arg1, arg2):
-  #   Your code goes here
-  return(arg1 / arg2)
+  return(arg1 / arg2 +1)
