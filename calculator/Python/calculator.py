14b63a3bd958d4a55e43694360e0b711a4657c3f
adrijuscoding <adrijus.zelinskis@gjensidige.no>
2022-05-04 12:20:17 +0300

    fix: calculator prep for merge conflict

--- calculator/Python/calculator.py
+++ calculator/Python/calculator.py
@@ -9,10 +9,3 @@ def subtract(arg1, arg2):
   #   Your code goes here
   return(arg1 - arg2)
 
-def multiply(arg1, arg2):
-  #   Your code goes here
-  return(arg1 * arg2)
-
-def divide(arg1, arg2):
-  #   Your code goes here
-  return(arg1 / arg2)
