2b6b80d5c57ef68d14a030f2e672325e62125803
Rita Milkerytė <r.milkeryte@gmail.com>
2022-05-04 12:44:32 +0300

    Pakeitimai pythono scripte

--- calculator/Python/calculator.py
+++ calculator/Python/calculator.py
@@ -1,18 +1,13 @@
 # Simple calculator Implementation in Python
 
-
 def add(arg1, arg2):
-  #   Your code goes here
   return((arg1 + arg2) / 2)
 
 def subtract(arg1, arg2):
-  #   Your code goes here
-  return(arg1 - arg2)
+  return(arg1 + arg2+ arg1)
 
 def multiply(arg1, arg2):
-  #   Your code goes here
   return(arg1 * arg2)
 
 def divide(arg1, arg2):
-  #   Your code goes here
   return(arg1 / arg2)
