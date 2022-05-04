dc87f5b8543f98736add16dc2465b36c7f4df27f
Jelena Motiejuniene <jelena.motiejuniene@gjensidige.lt>
2022-05-04 12:44:30 +0300

    Pakeitimai pythono scripte

--- calculator/Python/calculator.py
+++ calculator/Python/calculator.py
@@ -2,17 +2,17 @@
 
 
 def add(arg1, arg2):
-  #   Your code goes here
-  return((arg1 + arg2) / 2)
+  result = arg1 + arg2
+  return(result)
 
 def subtract(arg1, arg2):
-  #   Your code goes here
-  return(arg1 - arg2)
+  result = arg1 - arg2
+  return(result)
 
 def multiply(arg1, arg2):
-  #   Your code goes here
-  return(arg1 * arg2)
+  result = arg1 * arg2
+  return(result)
 
 def divide(arg1, arg2):
-  #   Your code goes here
-  return(arg1 / arg2)
+  result = arg1 / arg2
+  return(result)
