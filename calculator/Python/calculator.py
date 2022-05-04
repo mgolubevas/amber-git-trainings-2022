de660b084cbd067c0e1cf489acc89b5339c1b221
Normante Virbickiene <normante.virbickiene@amber.host>
2022-05-04 12:44:51 +0300

    Pakeitimai pytjono kode

--- calculator/Python/calculator.py
+++ calculator/Python/calculator.py
@@ -2,17 +2,16 @@
 
 
 def add(arg1, arg2):
-  #   Your code goes here
-  return((arg1 + arg2) / 2)
+   return((arg1 + arg2) / 2)
 
 def subtract(arg1, arg2):
-  #   Your code goes here
-  return(arg1 - arg2)
+   return(arg1 - arg2)
 
 def multiply(arg1, arg2):
-  #   Your code goes here
-  return(arg1 * arg2)
+   return(arg1 * arg2)
 
 def divide(arg1, arg2):
-  #   Your code goes here
-  return(arg1 / arg2)
+   return(arg1 / arg2)
+
+def divide(arg3, arg4):
+   return(arg3 / arg4)
